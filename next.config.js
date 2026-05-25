9c2ee515a2d069f6255b9f67a3c9a422c14ccf26
Adam Charest <matthewwweb@gmail.com>
2026-05-25 15:39:54 -0400

    Redirect old called URL to contact page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/called",
+        destination: "/contact-us",
+        permanent: true,
+      },
+    ];
+  },
+
   async headers() {
     return [
       {
