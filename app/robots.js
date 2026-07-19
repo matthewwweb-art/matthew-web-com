6733fc799efe130a8c47925c0c0cd3d6de3cd185
Adam Charest <matthewwweb@gmail.com>
2026-07-19 02:58:53 -0400

    Add robots file for search engines

--- app/robots.js
+++ app/robots.js
@@ -1,10 +1,13 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/admin"],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/admin/", "/api/"],
+      },
+    ],
     sitemap: "https://matthew-web.com/sitemap.xml",
+    host: "https://matthew-web.com",
   };
 }
