e0e79cc26c884ba438098b6c77dd8c135072ed01
Adam Charest <matthewwweb@gmail.com>
2026-09-10 06:48:07 -0400

    Fix mobile header alignment and update footer network links

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -2099,8 +2099,8 @@ export default function SiteHeader() {
             /* PHONE ONLY:
                moves MW emblem down and right
                without changing laptop or tablet */
-            left: 4px;
-            transform: translateY(2px);
+            left: 1px;
+            transform: translateY(-1px);
           }
 
           .mwx-energy.orange {
